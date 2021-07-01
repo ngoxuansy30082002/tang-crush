64bfdcea5f2ffd72758bb2ef59aff0d29062c47d
ngoxuansy30082002 <86767962+ngoxuansy30082002@users.noreply.github.com>
2021-07-01 22:39:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Yến  à! 😘',
+    introDesc: `Yêu một người không khó..
+Vì chẳng cần lí do..
+Chút tình cảm nho nhỏ..
+Chỉ dành cho cậu thôi 😘..`,
     btnIntro: 'hihi',
     title: 'Do you love me?? :)) 😙',
     btnYes: 'có 😘😘😍 <33',
     btnNo: 'Không 😒😒  :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'iu thì iu k iu thì iu 😆',
+    reply: 'iu thì iu mà k iu thì iu 😆',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Lời yêu đối với t thật khó nói... nhưng yêu m là điều mà t chắc chắn ❤ , chỉ mong m cảm nhận được thôi 😉.. yêu m rất nhiều.. rất rất nhiều .. ❤❤❤❤❤❤❤❤❤❤❤.. CHÚC NGỦ NGON MYLOVE ❤',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100012116410324'
+    messLink: 'https://i.pinimg.com/originals/f2/01/51/f20151a1f7e003426ca7f406b6f76c82.gif'
 }
